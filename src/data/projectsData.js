ebc7f4b89ef5c91c57754a2721948ae9ee03922d
Rayhan <mohammadrayhan796@gmail.com>
2026-06-25 01:25:59 +0600

    Update project images and live links for GreenNest, CleanTogether, LessonVault

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,29 +1,33 @@
+import greennest from '../assets/greennest.png';
+import clean from '../assets/clean.png';
+import lesson from '../assets/lesson.png';
+
 export const projectsData = [
   {
     id: 1,
     title: 'Green Nest House',
     description: 'A sustainable real estate platform connecting eco-friendly homes with conscious buyers.',
-    image: 'https://i.ibb.co/9kpVMV81/Screenshot-2026-01-05-170230.png',
+    image: greennest,
     tech: ['React', 'Firebase', 'Tailwind'],
     github: 'https://github.com/RAYHAN-HEXA/Green-nest-House',
-    demo: 'https://green-nest-house.netlify.app/'
+    demo: 'https://green-nest-house.vercel.app/'
   },
   {
     id: 2,
     title: 'CleanTogether Community',
     description: 'A community-driven platform promoting environmental awareness and clean living through shared resources and events.',
-    image: 'https://i.ibb.co/cSr1VNLn/Screenshot-2026-01-05-170119.png',
+    image: clean,
     tech: ['React', 'Node.js', 'MongoDB'],
     github: 'https://github.com/RAYHAN-HEXA/clean-together-client',
-    demo: 'https://clean-together-community.netlify.app'
+    demo: 'https://clean-together-client.vercel.app/'
   },
   {
     id: 3,
     title: 'LessonVault',
     description: 'An online learning management system (LMS) for educators to create, manage, and deliver courses effectively.',
-    image: 'https://i.ibb.co/4nJqpPhc/Screenshot-2026-01-05-165944.png',
+    image: lesson,
     tech: ['React', 'Node.js', 'MongoDB'],
     github: 'https://github.com/RAYHAN-HEXA/LessonVault-Client',
-    demo: 'https://lesson-vault-client.netlify.app/'
+    demo: 'https://lesson-vault-client.vercel.app/'
   }
 ];
